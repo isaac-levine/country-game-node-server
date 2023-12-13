6261d1ed59e3083156f3b581920f16d653107837
bengardner1 <bengardner433@gmail.com>
2023-12-13 14:37:53 -0500

    changed schema slighly

--- likes/schema.js
+++ likes/schema.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const schema = new mongoose.Schema(
   {
-    uId : {type : String, required : true},
+    userId : {type : String, required : true},
     countryCode: {type : String, required : true},
     haveTraveledTo: {type : Boolean, required : true},
     onBucketList: {type : Boolean, required : true},
